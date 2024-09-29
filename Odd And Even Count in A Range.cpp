a94fe15d03d37d505e973ac54f1b8c320d456553
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2024-09-29 15:24:33 +0600

    Add files via upload

---
+++ Odd And Even Count in A Range.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    int L = 1;
+    int R = 10;
+
+
+    int odd = ((R-L)/2)+1;
+    int even = (R-L)/2;
+
+    cout<<odd<<endl;
+    cout<<even<<endl;
+}
